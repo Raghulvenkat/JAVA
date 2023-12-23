3fe13b1bdb847dea9cea0dc834e6d095f041e4ac
Raghulvenkat <raghulvenkat007@gmail.com>
2023-12-24 00:19:51 +0530

    sample java amigoscode

---
+++ AmigoscodeSampleProgram1.java
@@ -0,0 +1,7 @@
+// This is a learning code in java
+package JAVA;
+public class AmigoscodeSampleProgram1{
+    public static void main(String[] args){
+        System.out.println("Hello\n");
+    }
+} 

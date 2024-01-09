758686ba98ee80bf9aa0512954acac84dad2f11c
Raghulvenkat <raghulvenkat007@gmail.com>
2024-01-09 16:06:04 +0530

    modified

--- AmingoscodeStringsExample.java
+++ AmingoscodeStringsExample.java
@@ -4,12 +4,13 @@ public class AmingoscodeStringsExample {
     public static void main(String[] args){
         //string examples
         char letter = 'A';
-        String name = new String("Raghul");
-        //Here String is a class and name is a object and lowercase is an attribute
+        String name = new String("Raghul");// there is no need to use 'new' everytime, we can omit it
+        //Here String is a class and name is a object and lowercase is an attribute/methods
         System.out.println(name.toLowerCase());
         System.out.println(name.charAt(1));// this method takes integer and giv the character based on index position
         System.out.println(name.contains("ghu"));// this check for the string present in the name
         String code = new String("code");
-        System.out.println(name.contains(code));
+        System.out.println(name.contains(code));// they give results as boolean
+        System.out.println(name.equals(code));//able to use only objects and returns as boolean
     }
 }

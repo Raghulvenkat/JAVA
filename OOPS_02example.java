a5387377577da2e2b032a2a2f265e72a1edc698e
Raghulvenkat <raghulvenkat007@gmail.com>
2024-01-25 00:36:25 +0530

    example file of OOPS_02 program

---
+++ OOPS_02example.java
@@ -0,0 +1,11 @@
+package JAVA;
+
+public class OOPS_02example {
+    public static void main(String [] args){
+
+    }
+    public static void message(){
+        System.out.println("This program file is inherited to OOPS_02.java");
+    }
+    
+}
